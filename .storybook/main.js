9fb20065917627db3935a78e3246848db4fcf10a
quezia <qfonseca1@gmail.com>
2021-11-25 17:25:47 -0300

    config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
+  ],
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,52 @@
+import { addDecorator } from "@storybook/react";
+import GlobalStyle from "../src/styles/GlobalStyle";
+import { BreakpointSize } from "../src/styles/Breakpoints";
+
+addDecorator((storyFn) => (
+  <>
+    <GlobalStyle />
+    {storyFn()}
+  </>
+));
+
+const customViewports = {
+  extraSmall: {
+    name: "Portrait phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+  small: {
+    name: "Landscape phone (sm)",
+    styles: {
+      width: "640px",
+      height: "360px",
+    },
+  },
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  large: {
+    name: "Laptop (lg)",
+    styles: {
+      width: "1024px",
+      height: "768px",
+    },
+  },
+  extraLarge: {
+    name: "Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +55,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: customViewports,
+  },
+};
