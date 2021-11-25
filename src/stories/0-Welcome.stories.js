77cd628c2ee6dc7bde0707cd636c034d55e8261b
quezia <qfonseca1@gmail.com>
2021-11-25 17:26:17 -0300

    Welcome Sotories doc

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toScheduleApp = () => (
+  <div>
+    <h1>Schedule App</h1>
+    <p></p>
+  </div>
+);
