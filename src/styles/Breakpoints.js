0e966c960f57b3e96b497e109e3046977ab5ed8b
quezia <qfonseca1@gmail.com>
2021-11-25 17:24:10 -0300

    Add Global styles

---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,8 @@
+export const BreakpointSize = {
+  sm: 578,
+  md: 768,
+  lg: 952,
+  xl: 1200,
+};
+
+export const breakAt = (size) => `@media (min-width: ${size}px)`;
---
+++ src/styles/Breakpoints.test.js
@@ -0,0 +1,10 @@
+import { BreakpointSize, breakAt } from "./Breakpoints";
+
+test.each([
+  [BreakpointSize.sm],
+  [BreakpointSize.md],
+  [BreakpointSize.lg],
+  [BreakpointSize.xl],
+])("break at %ipx", (size) => {
+  expect(breakAt(size)).toEqual(`@media (min-width: ${size}px)`);
+});
