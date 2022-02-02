fe4b2becf3c350c50b21f509dfcd43338194dfd1
ZacaDev23 <diego.z.s.de.lima@gmail.com>
2022-02-02 00:58:30 -0300

    Update product GGrid test

--- src/components/organisms/ProductGrid.test.js
+++ src/components/organisms/ProductGrid.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "test-utils";
+import { MemoryRouter } from "react-router-dom";
 
 import ProductGrid from "./ProductGrid";
 
@@ -20,13 +21,21 @@ describe.each([
   [6, 3],
 ])("with %i products", (size, expected) => {
   test(`show only ${expected}`, () => {
-    render(<ProductGrid products={buildProducts(size)} />);
+    render(
+      <MemoryRouter>
+        <ProductGrid products={buildProducts(size)} />
+      </MemoryRouter>
+    );
 
     expect(screen.getAllByRole("heading").length).toBe(expected);
   });
 
   test("show all the products when button is clicked", async () => {
-    render(<ProductGrid products={buildProducts(size)} />);
+    render(
+      <MemoryRouter>
+        <ProductGrid products={buildProducts(size)} />
+      </MemoryRouter>
+    );
 
     await fireEvent.click(screen.getByText("Lista Completa"));
 
